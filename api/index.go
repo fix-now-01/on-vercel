d6a8bb446fde729c27696630d3b22f8162f3401c
Your Name <you@example.com>
2025-07-14 07:18:01 +0000

    test

--- api/index.go
+++ api/index.go
@@ -5,7 +5,11 @@ import (
   "net/http"
 )
  
-func Handler(w http.ResponseWriter, r *http.Request) {
+func Wayback(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+}
+
+func Playback(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
 
